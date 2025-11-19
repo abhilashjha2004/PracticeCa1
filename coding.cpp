eae9e551f78892b609349b4ede4b11e822a75abf
Abhilash Jha <abhilashjha2004@gmail.com>
2025-11-19 23:34:02 +0530

    add one more loop

--- coding.cpp
+++ coding.cpp
@@ -3,9 +3,13 @@ using namespace std;
 
 int main(){
     string s = "Abhilash Jha";
+    int n=5;
 	cout << s << endl;
 	cout << "Lovenish" << endl;
     for (char ch : s){
         cout << ch << endl;
+    }
+    for (int i=1; i<=n; i++){
+        cout << i << " ";
     }
 }
